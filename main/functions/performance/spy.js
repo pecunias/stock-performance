2d2d2ea4b9a3f490ea52080c51ff03376073174e
pecunias <jelte_wiersma@hotmail.com>
2020-10-26 17:20:48 +0100

    remove unused value

--- main/functions/performance/spy.js
+++ main/functions/performance/spy.js
@@ -9,10 +9,8 @@ const getPerformance = async (initialValue, startDate) => {
         from: startDate,
         to: today,
       }, function (err, result) {
-        const amountSPY = Math.floor(initialValue / (result[result.length - 1].close));
         result.forEach((element) => {
             const date = `${element.date.getFullYear()}-${(element.date.getMonth() + 1)}-${element.date.getDate()}`;
-            const amount = amountSPY;
             const entry = {DATE: date, VALUE: element.close};
             results.push(entry);
         });
